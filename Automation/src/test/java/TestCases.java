41c2637913410838db7907b987022bb8a1b43cd8
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-12-03 15:45:49 +0530

    Annotation Add

--- Automation/src/test/java/TestCases.java
+++ Automation/src/test/java/TestCases.java
@@ -1,14 +1,15 @@
+import org.testng.annotations.Test;
+
 public class TestCases {
-
-
+    @Test
     void setup(){
         System.out.println("I am inside setup");
     }
-
+    @Test
     void testSteps(){
         System.out.println("I am inside testSteps");
     }
-
+    @Test
     void tearDown(){
         System.out.println("I am inside tearDown");
     }
