0ad1af58fced9063263083738ecb814f3203303b
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-12-03 15:24:30 +0530

    TestNG

---
+++ Automation/src/main/java/TestCases.java
@@ -0,0 +1,15 @@
+public class TestCases {
+
+
+    void setup(){
+        System.out.println("I am inside setup");
+    }
+
+    void testSteps(){
+        System.out.println("I am inside testSteps");
+    }
+
+    void tearDown(){
+        System.out.println("I am inside tearDown");
+    }
+}
